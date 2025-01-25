ea3e881263d0699cc3e4d617390f376c832ee44b
Monica15268 <monicamomosoy2694@gmail.com>
2025-01-24 23:27:57 -0500

    Agrego de obstaculos y mejoramiento grafico del laberinto

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Runtime.CompilerServices;
-
+using System.Text;
+using Spectre.Console;
 public class Laberinto
 {
     public static int anchodellaberinto = 21;
@@ -43,7 +45,7 @@ public class Laberinto
 
 
 
-        for (int i = 0; i < anchox.Length; i++)
+        for (int i = 0; i < anchox.Length; i++) //una sola porque ambas tienen la misma extension
         {
             int nuevomovimientoX = ancho + anchox[i];
             int nuevomovimientoY = largo + largoY[i];
@@ -64,7 +66,7 @@ public class Laberinto
     {
 
         int casillainicial = random.Next(1, anchodellaberinto - 1) / 2 * 2 + 1;
-        int casillafinal = random.Next(1, largodellaberinto - 2) / 2 * 2 + 1;
+        int casillafinal = random.Next(1, largodellaberinto - 2) / 2 * 2 + 1; 
         Expansiondecaminos(casillainicial, casillafinal);//empezar desde aca
         Maze[1, 1] = 0;
         Maze[anchodellaberinto - 1, largodellaberinto - 2] = 0;
@@ -72,16 +74,17 @@ public class Laberinto
 
     public static void RecorrerMaze()
     {
-
+        
+        
         int anchodelaconsola = Console.WindowWidth;
         int largodelaconsola = largodellaberinto;
-        int posiciondellaberinto = (anchodelaconsola - largodelaconsola) / 2;
-
+        int posiciondellaberinto = (anchodelaconsola - largodelaconsola) / 2; //centrar el laberinto en el medio
+        StringBuilder Introducirlaberintodentrodelpanel = new StringBuilder();
         for (int i = 0; i < anchodellaberinto; i++)
         {
             Console.Write(new string(' ', posiciondellaberinto));
             for (int j = 0; j < largodellaberinto; j++)
-            {    if(Maze[i, j] == 1)
+            {    if(Maze[i, j] == 1) //diseñar laberinto
                 {
                     Console.BackgroundColor= ConsoleColor.DarkMagenta;
                     Console.ForegroundColor= ConsoleColor.DarkMagenta;
@@ -101,20 +104,35 @@ public class Laberinto
                     Console.BackgroundColor = ConsoleColor.DarkGray;
                     Console.ForegroundColor = ConsoleColor.DarkGray;
                 }
-                Console.Write(Maze[i, j] + " " );
-
+                if (Maze[i, j] == 4){
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                }
+                Console.Write(Maze[i, j] + " ");
+                
                
                 Console.ResetColor();
             }
             Console.WriteLine();
+            
         }
+        //var panel = new Panel("Land");
+        //{ 
+        //panel.Width = largodellaberinto * 2;
+        //panel.Height = anchodellaberinto * 2;
+        //panel.Header = new PanelHeader("Laberinto");
+        //panel.Border = BoxBorder.Heavy;
+        //}
+        //panel.Add(new Text(Introducirlaberintodentrodelpanel.ToString()));
+
+        //AnsiConsole.Write(panel);
     }
-    public static void Piedras(int piedra)
+    public static void Piedras(int piedra) //obstaculos
     {
         int cantidadobstaculos = 9;
         int posdelobstaculoX;
         int posdelobstaculoY;
-        for(int i = 0;i < cantidadobstaculos; i++)
+        for(int i = 0;i < cantidadobstaculos; i++) //ciclo para que me ponga la cantidad de piedras que le indique
         {
             do
             {
@@ -127,7 +145,7 @@ public class Laberinto
             Maze[posdelobstaculoX, posdelobstaculoY] = piedra;
         }
     }
-    public static void Esmeraldas(int cantidadesmeraldas, int esmeralda)
+    public static void Esmeraldas(int cantidadesmeraldas, int esmeralda) //lo que debe recoger el jugador
     {
         int positionesmeraldaX;
         int positionesmeraldaY;
@@ -143,12 +161,76 @@ public class Laberinto
             Maze[positionesmeraldaX, positionesmeraldaY] = esmeralda;
         }
     }
-    public static void Main(string[] args)
-    {
+    public static void Seleccion(){
+        List<Characters> personajes = new List<Characters>();
+        {
+            new Characters("Goro", 60, 1 );
+            new Characters("Paco", 70,  3 );
+            new Characters("Juan", 100, 7 );
+        }
+        Console.WriteLine("Elige un personaje");
+        for(int i = 0;i < personajes.Count; i++)
+        {
+            Console.Write($"{i + 1}.{personajes[i].Nombre}"  + personajes[i].Vida);
+        }
+        int seleccionadoone;
+        int seleccionadotwo;
+        while (true)
+        {
+            seleccionadoone = Convert.ToInt32(Console.ReadLine());
+            seleccionadotwo = Convert.ToInt32(Console.ReadLine());
 
+            if(seleccionadoone > 0 && seleccionadoone < personajes.Count && seleccionadotwo> 0 && seleccionadotwo < personajes.Count) {
+                break;
+            }
+            Console.WriteLine("Introduce otro numero");
+        }
+            
+        Characters seleccion1 = personajes[seleccionadoone - 1];
+        Characters seleccion2 = personajes[seleccionadotwo - 1];
+}
+    public static void Selecciondel1eerpersonaje(int seleccion1)
+    {
+        
+        int posicionseleccion1;
+        int posicionseleccion2;
+        do
+        {
+            posicionseleccion1 = random.Next(1, 3);
+            posicionseleccion2 = random.Next(1, 3);
+
+            Maze[posicionseleccion1, posicionseleccion2] = seleccion1;
+        }
+        while (Maze[posicionseleccion1, posicionseleccion2] != 0);
+
+
+    }
+
+    public static void Selecciondel1eerpersonaje2(int seleccion2)
+    {
+       
+        int posicionseleccion1;
+        int posicionseleccion2;
+        do
+        {
+            posicionseleccion1 = random.Next(1, 3);
+            posicionseleccion2 = random.Next(1, 3);
+
+            Maze[posicionseleccion1, posicionseleccion2] = seleccion2;
+        }
+        while (Maze[posicionseleccion1, posicionseleccion2] != 0);
+
+
+    }
+
+    public static void Main(string[] args) //basicamente como funciona el proyecto
+    {
+       
         Generaciondellaberinto();
         Iniciodellaberinto();
         Esmeraldas(15, 5);
+        Selecciondel1eerpersonaje(3);
+        Selecciondel1eerpersonaje2(9);
         Piedras(7);
         RecorrerMaze();
         
@@ -156,5 +238,42 @@ public class Laberinto
 
 }
 
+
+public class Characters
+{
+
+    public string Nombre { get; set; }
+    public int Vida { get; set; }
+    public int Velocidad {  get; set; }
+    public int Saltatrampas { get; set; }
+    public int Enfriamiento { get; set; }
+    //public int PosicionGorox { get; set; }
+    //public int PosicionGoroy { get; set; }
+
+    public Characters(string nombre, int vida,   int velocidad)
+    {
+    
+        Nombre = nombre;
+        Vida = vida;
+        //Enfriamiento = enfriamiento;
+        //Saltatrampas = saltartrampas;
+        //PosicionGorox = posiciongorox;
+        //PosicionGoroy = posiciongoroy;
+        Velocidad = velocidad;
+    }
+   
+}
+
+
+
+public class Charatcer
+{
+    public string Nombre { get; set; }
+    public int Vida { get; set; }
+    public int Rapidez { get; set; }
+    public int Enfriamiento { get; set; }
+    public int PosicionMariano { get; set; }
+    public int PosicionMarianoy { get; set; }
+}
 
 
